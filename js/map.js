2b44a13f5b958c2f522ee11293a2cb9ebe60f533
TracyLi025 <jiawenli025@gmail.com>
2018-11-26 18:10:28 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initMap(){
   var home = {lat: 20.024632, lng: 110.309653};
   var middeschool = {lat: 20.027554, lng: 110.306955};
-  var highschool = {lat: , lng: };
+  var highschool = {lat: 19.997098, lng: 110.346879};
   var park = {lat: 20.033708, lng: 110.315855};
   var marina = {lat: 20.041741, lng: 110.229879};
   var map = new google.maps.Map(
